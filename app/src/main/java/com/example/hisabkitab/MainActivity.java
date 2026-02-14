5c186aa45392ebc2e698cfe6926c357a5e9a93ab
Manish Kc <manishkc2004@gmail.com>
2026-02-14 10:00:07 +0545

    Everything fresh

---
+++ app/src/main/java/com/example/hisabkitab/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.hisabkitab;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.login);
+    }
+}
