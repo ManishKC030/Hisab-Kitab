9aaa7e3a2306329533764189cf4806030bd84ab3
Manish Kc <manishkc2004@gmail.com>
2026-02-14 12:26:57 +0545

    Pages Linked with Eachother

--- app/src/main/java/com/example/hisabkitab/LoginActivity.java
+++ app/src/main/java/com/example/hisabkitab/LoginActivity.java
@@ -1,10 +1,27 @@
 package com.example.hisabkitab;
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+
 public class LoginActivity extends Activity {
+    TextView txtSignUp;
+
     @Override
     protected void onCreate(Bundle b) {
         super.onCreate(b);
-        setContentView(R.layout.login); // your login layout
+        setContentView(R.layout.login);
+
+        txtSignUp = findViewById(R.id.txtsignup);
+        txtSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish(); //closes login page
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hisabkitab/RegisterActivity.java
+++ app/src/main/java/com/example/hisabkitab/RegisterActivity.java
@@ -1,11 +1,27 @@
 package com.example.hisabkitab;
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class RegisterActivity extends  Activity{
+    TextView txtLogin;
+
     @Override
     protected void onCreate(Bundle b) {
         super.onCreate(b);
         setContentView(R.layout.register);
+
+        txtLogin = findViewById(R.id.txtlogin);
+        txtLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); //closes register page
+            }
+        });
     }
 }
