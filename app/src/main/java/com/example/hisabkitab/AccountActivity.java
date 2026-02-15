398b4240120fcb1c671256f1464c35c20604f9e0
Manish Kc <manishkc2004@gmail.com>
2026-02-15 15:57:34 +0545

    Not Much Not Less

--- app/src/main/java/com/example/hisabkitab/AccountActivity.java
+++ app/src/main/java/com/example/hisabkitab/AccountActivity.java
@@ -1,4 +1,51 @@
 package com.example.hisabkitab;
 
-public class AccountActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class AccountActivity extends Activity {
+
+    TextView txtName, txtEmail;
+    Button btnLogout;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.account);
+
+        txtName = findViewById(R.id.txtName);
+        txtEmail = findViewById(R.id.txtEmail);
+        btnLogout = findViewById(R.id.btnLogout);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user != null) {
+            txtEmail.setText(user.getEmail());
+
+            if (user.getDisplayName() != null) {
+                txtName.setText(user.getDisplayName());
+            } else {
+                txtName.setText("Welcome User");
+            }
+        }
+
+        btnLogout.setOnClickListener(v -> {
+
+            mAuth.signOut();
+
+            Intent intent = new Intent(AccountActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        });
+    }
 }
+
--- app/src/main/java/com/example/hisabkitab/DashboardActvity.java
+++ app/src/main/java/com/example/hisabkitab/DashboardActvity.java
@@ -2,15 +2,24 @@ package com.example.hisabkitab;
 
 import android.os.Bundle;
 import android.app.Activity;
-
+import android.content.Intent;
+import android.widget.*;
 
 public class DashboardActvity extends Activity {
+    LinearLayout navBtnAccount;
 
     @Override
     protected void onCreate(Bundle b) {
         super.onCreate(b);
         setContentView(R.layout.dashboard);
+        navBtnAccount = findViewById(R.id.navBtnAccount);
 
+        navBtnAccount.setOnClickListener(v -> {
+
+            Intent intent = new Intent(DashboardActvity.this, AccountActivity.class);
+            startActivity(intent);
+
+        });
 
     }
 }
